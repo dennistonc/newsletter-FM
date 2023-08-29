9bd036266419a178848925849877c57b03dd39f6
dennistonc <dennistonc@gmail.com>
2023-08-28 22:54:00 -0500

    remade newsletter sign up from frontend mentor, just need js valid/error checking and /GET

---
+++ newsletter.js
@@ -0,0 +1,17 @@
+// Button Logic
+submitEmail = () => {
+    document.getElementById("showSuccess").style.display = "block";
+    document.getElementById("showReset").style.display = "none";
+  }
+
+dismissEnd = () => {
+    document.getElementById("showSuccess").style.display = "none";
+    document.getElementById("showReset").style.display = "block";
+} 
+
+// Email Validity
+
+
+
+
+// Email Parameters
